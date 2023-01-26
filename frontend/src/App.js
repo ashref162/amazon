2fe1aaac739c7e162a28c8b6337290e571716ddc
ashref162 <ashref717732326@gmail.comm>
2023-01-26 18:59:30 +0300

    video 3

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Wellcom To Ashraf Web Sit</a>
+      </header>
+      <main>list of rducts</main>
+    </div>
+  );
+}
+
+export default App;
